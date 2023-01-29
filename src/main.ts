3a7301500e6d7d28d5ec1bca2977173193fee3a4
Yuri Pieters <magejohnyjtp@gmail.com>
2023-01-29 13:43:04 +0000

    refactor: clean up main.ts a little

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ import dedent from "ts-dedent";
 
 import { name, version, description } from "../package.json";
 
-import type { VolumeOverview, ContentInfo, Chapter } from "./types";
+import type { VolumeOverview, ContentInfo } from "./types";
 
 const escapeString = (string: string): string =>
   string.replace(/[=;#\\\n]/g, "$&");
 
-const formatChapterMetadata = (chapters: Chapter[], endTimestamp: string) =>
-  chapters
+const formatChapterMetadata = (contentInfo: ContentInfo) =>
+  contentInfo[0][0]
     .reduce<{ title: string; start: string; end?: string }[]>((arr, chap) => {
       const [title, start] = chap;
       if (start === undefined) {
@@ -20,7 +20,7 @@ const formatChapterMetadata = (chapters: Chapter[], endTimestamp: string) =>
         return [{ title, start: "0" }];
       }
 
-      const last = arr[arr.length - 1];
+      const last = arr.pop();
 
       if (last === undefined) {
         throw Error("Unexpected chapters structure");
@@ -28,24 +28,22 @@ const formatChapterMetadata = (chapters: Chapter[], endTimestamp: string) =>
 
       if (last.start === start) {
         // The last chapter and this chapter have the same timestamp, combine them
-        return [
-          ...arr.slice(0, -1),
-          { title: `${last.title} - ${title}`, start },
-        ];
+        return [...arr, { title: `${last.title} - ${title}`, start }];
       }
 
       // Append the chapter and update the end time of the last chapter
-      return [...arr.slice(0, -1), { ...last, end: start }, { title, start }];
+      return [...arr, { ...last, end: start }, { title, start }];
     }, [])
     .map(
-      ({ title, start, end = endTimestamp }) => dedent`
+      ({ title, start, end = contentInfo[1] }) => dedent`
         [CHAPTER]
         TIMEBASE=1/1000
         START=${start}
         END=${end}
         title=${escapeString(title)}
       `
-    );
+    )
+    .join("\n");
 
 const formatBookMetadata = (volumeOverview: VolumeOverview) =>
   (
@@ -58,13 +56,24 @@ const formatBookMetadata = (volumeOverview: VolumeOverview) =>
       ["composer", volumeOverview[1][16][3].join(", ")],
       ["genre", "Audiobook"],
     ] as const
-  ).map((entry) => `${entry[0]}=${escapeString(entry[1])}`);
+  )
+    .map((entry) => `${entry[0]}=${escapeString(entry[1])}`)
+    .join("\n");
+
+const formatFFMetadata = (chapterMeta: string, bookMeta: string) => dedent`
+  ;FFMETADATA
+  ${bookMeta}
+
+  ${chapterMeta}
+`;
 
 async function scrape(file: string) {
   const dom = await JSDOM.fromFile(file, { runScripts: "dangerously" });
 
-  const contentInfo = dom.window['_OC_contentInfo'] as ContentInfo | undefined;
-  const volumeOverview = dom.window['_OC_volumeOverview'] as VolumeOverview | undefined;
+  const contentInfo = dom.window["_OC_contentInfo"] as ContentInfo | undefined;
+  const volumeOverview = dom.window["_OC_volumeOverview"] as
+    | VolumeOverview
+    | undefined;
 
   dom.window.close();
 
@@ -72,14 +81,10 @@ async function scrape(file: string) {
     throw Error("Could not extract metadata from the page");
   }
 
-  const chapterMetadata = formatChapterMetadata(contentInfo[0][0], contentInfo[1])
+  const chapterMetadata = formatChapterMetadata(contentInfo);
   const bookMetadata = formatBookMetadata(volumeOverview);
 
-  stdout.write(dedent`
-    ;FFMETADATA
-    ${bookMetadata.join("\n")}
-    ${chapterMetadata.join("\n")}
-  `);
+  return formatFFMetadata(chapterMetadata, bookMetadata);
 }
 
 const program = new Command();
@@ -89,14 +94,14 @@ program
   .version(version)
   .description(description)
   .argument("<file>")
-  .action(scrape);
+  .action(async (file: string) => {
+    stdout.write(await scrape(file));
+  });
 
 function main() {
-  program.parseAsync().catch(
-    (reason) => {
-      throw Error(reason)
-    }
-  )
+  program.parseAsync().catch((reason) => {
+    throw Error(reason);
+  });
 }
 
 export default main;
